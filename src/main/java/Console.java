560c65164921c9c991a98516772e1c4f48a32ec8
ramzes <loginovvv01@gmail.com>
2021-06-24 23:52:00 +0300

    final_fake

--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -24,7 +24,19 @@ public class Console {
             System.out.println("Введите точность (количество точек)");
             DifferentialEquation.n = scanner.nextInt();
             DifferentialEquation.advancedEulerMethod1();
-            DifferentialEquation.searchY();
+            System.out.println("Шаг: " + DifferentialEquation.step);
+            System.out.println("Количество шагов: " + DifferentialEquation.n);
+            System.out.println("Таблица:");
+            for (int i = -1; i < DifferentialEquation.n; i++) {
+                if (i == -1) {
+                    System.out.format("| %2s | %8s | %10s | %10s |", "i", "x", "y", "y'");
+                    System.out.println();
+                    System.out.println("-------------------------------------------");
+                }else {
+                    System.out.format("| %-2d | %-8f | %-10f | %-10f |", i, DifferentialEquation.xInter[i], DifferentialEquation.ySimple[i], DifferentialEquation.yInter[i]);
+                    System.out.println();
+                }
+            }
 
         } else if (type == 2) {
             System.out.print("Введите начальные условия\nx0 = ");
@@ -42,7 +54,19 @@ public class Console {
             System.out.println("Введите точность (количество точек)");
             DifferentialEquation.n = scanner.nextInt();
             DifferentialEquation.advancedEulerMethod2();
-            DifferentialEquation.searchY();
+            System.out.println("Шаг: " + DifferentialEquation.step);
+            System.out.println("Количество шагов: " + DifferentialEquation.n);
+            System.out.println("Таблица:");
+            for (int i = -1; i < DifferentialEquation.n; i++) {
+                if (i == -1) {
+                    System.out.format("| %2s | %8s | %10s | %10s |", "i", "x", "y", "y'");
+                    System.out.println();
+                    System.out.println("-------------------------------------------------");
+                }else {
+                    System.out.format("| %-2d | %-8f | %-14f | %-14f |", i, DifferentialEquation.xInter[i], DifferentialEquation.ySimple[i], DifferentialEquation.yInter[i]);
+                    System.out.println();
+                }
+            }
 
         } else {
             System.out.println("Вы ввели некорректный тип уравнения :)");
--- src/main/java/DifferentialEquation.java
+++ src/main/java/DifferentialEquation.java
@@ -16,6 +16,10 @@ public class DifferentialEquation {
     static double[] xPol;
     static double[] yPol;
     static int sumX;
+    static double step;
+    static double[] xInter;
+    static double[] yInter; // производная
+    static double[] ySimple; // НЕ производная
 
 
     static void advancedEulerMethod1() {
@@ -30,20 +34,22 @@ public class DifferentialEquation {
             p++;
         }
 
-        XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
 
         double h = (rightBoard - x0) / n;
-        /* интерполяция бляха....... */
+        step = h;
 
-        double[] xInter = new double[n + 1];
-        double[] yInter = new double[n + 1];
+        xInter = new double[n + 1];
+        yInter = new double[n + 1];
+        ySimple = new double[n + 1];
         xInter[0] = x0;
         yInter[0] = y0;
+        ySimple[0] = y0;
         p = 1;
         for (x = x0 + h; x <= rightBoard; x = x + h) {
             y = y0 + (h / 2.0) * (Function.function1XY(x0, y0) + Function.function1XY(x, y0 + h * Function.function1XY(x0, y0)));
             xInter[p] = x;
             yInter[p] = y;
+            ySimple[p] = Function.function1X(x);
 
             x0 = x;
             y0 = y;
@@ -79,11 +85,19 @@ public class DifferentialEquation {
 
             xxx = xxx + 0.1;
         }
-        chart.addSeries("interpol", xPol, yPol);
-
+        XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y'(x)", xPol, yPol);
 
         new SwingWrapper(chart).displayChart();
     }
+
+
+
+
+
+
+
+
+
 
     static void advancedEulerMethod2() {
         leftBoard = x0;
@@ -97,20 +111,22 @@ public class DifferentialEquation {
             p++;
         }
 
-        XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
 
         double h = (rightBoard - x0) / n;
-        /* интерполяция бляха....... */
+        step = h;
 
-        double[] xInter = new double[n + 1];
-        double[] yInter = new double[n + 1];
+        xInter = new double[n + 1];
+        yInter = new double[n + 1];
+        ySimple = new double[n + 1];
         xInter[0] = x0;
         yInter[0] = y0;
+        ySimple[0] = y0;
         p = 1;
         for (x = x0 + h; x <= rightBoard; x = x + h) {
             y = y0 + (h / 2.0) * (Function.function2XY(x0, y0) + Function.function2XY(x, y0 + h * Function.function2XY(x0, y0)));
             xInter[p] = x;
             yInter[p] = y;
+            ySimple[p] = Function.function2X(x);
             x0 = x;
             y0 = y;
             p++;
@@ -145,27 +161,11 @@ public class DifferentialEquation {
 
             xxx = xxx + 0.1;
         }
-        chart.addSeries("interpol", xPol, yPol);
+
+        XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y'(x)", xPol, yPol);
 
 
         new SwingWrapper(chart).displayChart();
-    }
-
-    public static void searchY() {
-        Scanner scanner1 = new Scanner(System.in);
-        while (true) {
-            System.out.println("Введите значение X для поиска значения Y по интерполяции:");
-            double x_t = scanner1.nextDouble();
-            if (x_t == -999) { break; }
-            for (int i = 0; i < sumX; i++) {
-                if (Math.abs(xPol[i] - x_t) <= 0.00001) {
-                    System.out.println("Значение Y в точке " + x_t + " равно " + yPol[i]);
-                }
-            }
-            System.out.println("Для завершения программы введите: -999");
-        }
-        System.out.println("Завершение программы...");
-        System.exit(0);
     }
 
     static boolean checkBoard() {
